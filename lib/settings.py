b887a541cc7c12c4d71ebdc58f755fff24ce6176
dddontshoot <32731888+dddontshoot@users.noreply.github.com>
2021-08-08 20:49:41 +1200

    Update settings.py

--- lib/settings.py
+++ lib/settings.py
@@ -2,7 +2,7 @@ def getSettings():      # This file stores the file locations for your computer.
     print("Importing settings")
     settings=dict()
 
-    settings["baseFile"]="mybase.base"             # Default file name only. Will be overridden by file names in the command line.
+    #settings["baseFile"]="mybase.base"             # Default file name only. Will be overridden by file names in the command line.
                                                    # baseFile should always have the extention .base to distinguish it from the working folder of the same name.
     settings["basePath"]="../script-output/"       # Filenames in the command line will be relative to this path. use settings["basePath"]="" for no path.
     settings["csvFile"]="assets/database_MasterList.csv" # Location of the reference database.
