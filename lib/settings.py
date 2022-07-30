03fab26a7baa9706eaa820b79fc9982a238f7af4
dddontshoot <32731888+dddontshoot@users.noreply.github.com>
2022-07-31 02:01:14 +1200

    Add files via upload

--- lib/settings.py
+++ lib/settings.py
@@ -5,10 +5,8 @@ def getSettings():      # This file stores the file locations for your computer.
     settings["baseFile"]="mybase.base"             # Default file name only. Will be overridden by file names in the command line.
                                                    # baseFile should always have the extention .base to distinguish it from the working folder of the same name.
     settings["basePath"]="../../script-output/"       # Filenames in the command line will be relative to this path. use settings["basePath"]="" for no path.
-    #settings["csvFile"]="../assets/database_MasterList.csv" # Location of the reference database.
-    #settings["csvFile"]="../assets/wallsgates.csv"
-    settings["csvFile"]="../assets/small.csv"
-    settings["meshPath"]="../Mesh/"                   # Location of the small entities to be imported into blender.
+    settings["csvFile"]="assets/database_MasterList.csv" # Location of the reference database.
+    settings["meshPath"]="Mesh/"                   # Location of the small entities to be imported into blender.
     settings["outputPath"]="output/"               # The output folder will house subfolders with a name that matches the .base file.
     settings["pathCharacter"]="/"                  # use "/" for Linux, or "\\" for Windows.
     #settings["pathCharacter"]="\\"                 # using the wrong symbol here might not stop the program from running, but it could result in some interesting filenames.
