d9406fd909a8049d57fef0d789040dade4a77ff8
dddontshoot <32731888+dddontshoot@users.noreply.github.com>
2022-08-03 07:08:48 +1200

    Added support for importing Collections

--- lib/blender.py
+++ lib/blender.py
@@ -40,8 +40,53 @@ def newObject(entityProperties, entityImportDetails, meshAdjustmentDatabase, col
     r = (entityProperties["direction"] * -45) + entityProperties["rotationAdjustment"]
     newObject.rotation_euler[2] = math.radians(r)
 
+def newInstance(entityProperties, entityImportDetails, meshAdjustmentDatabase, collection):
+
+    ##### I'm using the name newObject, but don't forget that 
+    ##### it's not an object, it's an instance.
+
+    import math
+    import json
+
+    # Create a new collection_instance which is just a copy of an empty collection.
+    bpy.ops.object.collection_instance_add(collection='Collection', align='WORLD', location=(0, 0, 0), scale=(1, 1, 1)) # well, it created an instance, but I don't know how to access it.
+    newObject=bpy.data.objects.get("Collection")
+
+    # Give it an appropriate name
+    newObject.name=entityProperties["name"]
+
+    # Add the object to a collection
+    collection.objects.link(newObject)
+
+    # Fill in all the custom properties
+    for item in entityProperties.keys():
+        newObject[item]=entityProperties[item]
+
+    # Mesh. Eventually, when we import the mesh, we'll need to know where to get it from.
+    # Lets check to see if the entity matches the criteria for mesh adjustment.
+    if entityProperties["name"] in meshAdjustmentDatabase.keys():
+        #print("found matching entity name, comparing for",entityProperties["direction"])
+        entityMeshAdjustment=json.loads(meshAdjustmentDatabase[entityProperties["name"]])
+        if str(entityProperties["direction"]) in entityMeshAdjustment.keys():
+            #print("replacing mesh...")
+            newObject["filename"] = entityMeshAdjustment[str(entityProperties["direction"])]
+        else:
+                newObject["filename"]=entityImportDetails["filename"]
+    else:
+                newObject["filename"]=entityImportDetails["filename"]
+
+    # Location
+    newObject.location = (entityProperties["x"], entityProperties["y"], 0)
+
+    # rotation
+    r = (entityProperties["direction"] * -45) + entityProperties["rotationAdjustment"]
+    newObject.rotation_euler[2] = math.radians(r)
+
 def collectionCreate(new, parent=bpy.context.scene.collection):
     print("Creating collection ",new)
+    
+    if type(parent) == str:
+        parent=bpy.data.collections.get(parent)
 
     myCollection = bpy.data.collections.new(new)   # yes, this is how you create a collection.
     parent.children.link(myCollection)             # The new collection resides in a void until this line links it in place.
@@ -58,6 +103,10 @@ def collectionRename(old,new):
     print("Renaming collection ",old,"to",new)
     item = bpy.data.collections.get(old)
     item.name=new
+
+def collectionSelect(colSurfaceName,colEntityName):
+    layer_collection = bpy.context.view_layer.layer_collection.children[colSurfaceName].children[colEntityName]
+    bpy.context.view_layer.active_layer_collection = layer_collection
 
 def deleteDefaultCube():
     print("Deleting the default objects.")
@@ -115,14 +164,14 @@ def buildEntity(datum):
 
                         unselectEverything()
 
-def concatCollection(datum):
-                        print("Importing" , datum)
+def concatCollection(fileName,collectionName):
+                        print("Importing" , fileName,"/",collectionName)
 
-                        section="\\Collection\\"
+                        section   = "\\Collection\\"
 
-                        filepath  = datum + section + "Collection"
-                        directory = datum + section
-                        filename  = "Collection"
+                        directory = fileName + section
+                        filename  = collectionName
+                        filepath  = directory+filename
 
                         bpy.ops.wm.append(
                         filepath=filepath, 
@@ -131,4 +180,3 @@ def concatCollection(datum):
                         )
 
                         unselectEverything()
-
